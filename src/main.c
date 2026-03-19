fce53aebb481a1ceeecda9490109a3a73ab14926
Krastti <salcev77@gmail.com>
2026-03-19 19:11:32 +0300

    update: update main.c from Windows

--- src/main.c
+++ src/main.c
@@ -1,21 +1,11 @@
 #include "../tests/unit_tests.h"
 #include "logging/logger.h"
-#include <stdio.h>
-#include <windows.h>
 
 int main() {
-    SetConsoleOutputCP(CP_UTF8);
-    SetConsoleCP(CP_UTF8);
-
     logger_init(LOG_DEBUG);
     LOG_DEBUG("Программа запущена");
 
-    printf("Включить тесты");
-    int value;
-    scanf("%d",&value);
-    if (value == 1) {
-      logger_set_level(LOG_ERROR);
-      run_all_tests();
-    }
+    logger_set_level(LOG_ERROR);
+    run_all_tests();
     return 0;
 }
