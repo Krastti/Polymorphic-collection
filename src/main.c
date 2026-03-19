c3d0d28b384d151e2c1a4db32d53838ea9a8e054
Krastti <salcev77@gmail.com>
2026-03-19 17:30:35 +0300

    update: update main.c from MacOS

--- src/main.c
+++ src/main.c
@@ -1,12 +1,8 @@
 #include "../tests/unit_tests.h"
 #include "logging/logger.h"
 #include <stdio.h>
-#include <windows.h>
 
 int main() {
-    SetConsoleOutputCP(CP_UTF8);
-    SetConsoleCP(CP_UTF8);
-
     logger_init(LOG_DEBUG);
     LOG_DEBUG("Программа запущена");
 
